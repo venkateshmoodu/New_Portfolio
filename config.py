741a2d2babb6a35302f11bde91b50dcc9df1e8f7
Moodu Venkatesh <venkateshmudu89@gmail.com>
2025-10-29 16:56:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
    
 SENDER_EMAIL = os.getenv('SENDER_EMAIL', 'mooduvenkatesh.nielit@gmail.com')
 SENDER_PASSWORD = os.getenv('lcypqqkyxtcjuxbq', '')
-RECIPIENT_EMAIL = os.getenv('RECIPIENT_EMAIL', 'venkatchouhan24@gmail.com')
+RECIPIENT_EMAIL = os.getenv('RECIPIENT_EMAIL', 'venkatchouhan24@gmail.com')list
